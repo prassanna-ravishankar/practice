4f6aeb725f6b97ee574ed4600c10f865246da2e8
masahi <masahi129@gmail.com>
2013-08-16 16:09:12 +0900

    fixed segfault

--- python/EigenWrap/wrapper_example.cpp
+++ python/EigenWrap/wrapper_example.cpp
@@ -13,14 +13,15 @@ class EigenWrap
 {
 public:
     EigenWrap(){}
-    void set_array(PyObject* numpy_array);
+    void set_array(PyArrayObject* numpy_array);
     PyObject* eigen_values();
 
 private:
-    MatrixXd mat;
+  //    Map<Matrix<double, Dynamic, Dynamic, RowMajor> > mat;
+  MatrixXd mat;
 };
 
-void EigenWrap::set_array(PyObject* numpy_array)
+void EigenWrap::set_array(PyArrayObject* numpy_array)
 {
     npy_intp *shape = PyArray_DIMS(numpy_array);
     mat = Map<Matrix<double, Dynamic, Dynamic, RowMajor> >(static_cast<double *> PyArray_DATA(numpy_array),shape[0], shape[1]);
@@ -31,14 +32,29 @@ PyObject* EigenWrap::eigen_values()
     SelfAdjointEigenSolver<MatrixXd> solver(mat);
     VectorXd eigen_values = solver.eigenvalues();
     npy_intp shape[] = {mat.rows()};
-    PyObject *ret = PyArray_SimpleNewFromData(1, shape, NPY_DOUBLE, eigen_values.data());
+    // PyObject *ret = PyArray_SimpleNewFromData(1, shape, NPY_DOUBLE, eigen_values.data());
+    PyObject *ret = PyArray_SimpleNew(1, shape, NPY_DOUBLE);
+    for(int i = 0; i < mat.rows(); ++i)
+      ((double *)PyArray_GETPTR1(ret, i))[0] = eigen_values(i);
+
     return ret;
 }
 
+void* extract_pyarray(PyObject* x)
+{
+	return x;
+}
+
+
 BOOST_PYTHON_MODULE(_EigenWrap)
 {
+  boost::python::converter::registry::insert(
+	    &extract_pyarray, boost::python::type_id<PyArrayObject>());
+  
     class_<EigenWrap>("EigenWrap")
         .def("set_array", &EigenWrap::set_array)
         .def("eigen_values", &EigenWrap::eigen_values)
     ;
+
+    import_array();
 }
--- python/wrapper_example.py
+++ python/wrapper_example.py
@@ -6,7 +6,7 @@ m = 10
 arr = np.random.rand(m,m)
 
 eigen = EigenWrap()
-eigen.set_array(arr)
-val,vec = np.linalg.eig(arr)
+eigen.set_array(np.dot(arr.T,arr))
+val,vec = np.linalg.eig(np.dot(arr.T,arr))
 print eigen.eigen_values()
 print val
